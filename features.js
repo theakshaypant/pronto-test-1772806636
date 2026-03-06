c650bcb99a1b9fc61f2de28e75aad36e142983a5
PROnto Test <test@pronto.local>
2026-03-06 19:48:48 +0530

    feat: add wave and high-five features

---
+++ features.js
@@ -0,0 +1,10 @@
+// Features module
+function wave(name) {
+  console.log(`👋 ${name}!`);
+}
+
+function highFive(name) {
+  console.log(`High five, ${name}! ✋`);
+}
+
+module.exports = { wave, highFive };
